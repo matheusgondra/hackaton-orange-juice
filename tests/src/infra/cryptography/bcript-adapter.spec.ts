8873c80f96011ac9c36a166c2cafa7a3e594ee79
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-23 11:57:21 -0300

    refactor: move sut creation to a factory method

--- tests/src/infra/cryptography/bcript-adapter.spec.ts
+++ tests/src/infra/cryptography/bcript-adapter.spec.ts
@@ -7,9 +7,14 @@ jest.mock("bcrypt", () => ({
 	}
 }));
 
+const makeSut = (): BcryptAdapter => {
+	const salt = 12;
+	return new BcryptAdapter({ salt });
+};
+
 describe("Bcrypt Adapter", () => {
 	it("Should call compare with correct values", () => {
-		const sut = new BcryptAdapter({ salt: 12 });
+		const sut = makeSut();
 		const compareSpy = jest.spyOn(bcrypt, "compare");
 		sut.compare({ value: "any_value", hash: "any_hash" });
 		expect(compareSpy).toHaveBeenCalledWith("any_value", "any_hash");
