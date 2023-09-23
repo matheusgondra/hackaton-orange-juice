65c486b9aa9beea26056d3835f3b51bb3fc28427
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-23 17:53:50 -0300

    refactor: move sut creation to a factory method

--- tests/src/application/middlewares/auth-middleware.spec.ts
+++ tests/src/application/middlewares/auth-middleware.spec.ts
@@ -1,9 +1,20 @@
 import { unauthorized } from "../../../../src/application/helpers";
 import { AuthMiddleware } from "../../../../src/application/middlewares";
 
+interface SutTypes {
+	sut: AuthMiddleware;
+}
+
+const makeSut = (): SutTypes => {
+	const sut = new AuthMiddleware();
+	return {
+		sut
+	};
+};
+
 describe("AuthMiddleware", () => {
 	it("Should return 403 if no access-token exists in headers", async () => {
-		const sut = new AuthMiddleware();
+		const { sut } = makeSut();
 		const request = {};
 		const httpResponse = await sut.handle(request);
 		expect(httpResponse).toEqual(unauthorized());
