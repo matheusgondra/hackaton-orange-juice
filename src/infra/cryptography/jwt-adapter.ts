ee560d162f8827497b8246a001632e1a25af7fc9
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 10:35:26 -0300

    feat: ensure JwtAdapter.decrypt call verify with correct values

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -1,7 +1,7 @@
-import { Encrypter } from "../../data";
+import { Decrypter, Encrypter } from "../../data";
 import jwt from "jsonwebtoken";
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
 	private readonly secret: string;
 
 	constructor(secret: string) {
@@ -10,5 +10,9 @@ export class JwtAdapter implements Encrypter {
 
 	async encrypt(value: string): Promise<string> {
 		return jwt.sign({ id: value }, this.secret);
+	}
+
+	async decrypt(cipherText: string): Promise<Decrypter.Result> {
+		return jwt.verify(cipherText, this.secret) as any;
 	}
 }
