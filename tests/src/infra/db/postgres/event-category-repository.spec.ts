1372de5c730819af4798ebbca64c8134eb32f5c6
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 15:15:30 -0300

    test: ensure EventCategoryRespository return false on failure

--- tests/src/infra/db/postgres/event-category-repository.spec.ts
+++ tests/src/infra/db/postgres/event-category-repository.spec.ts
@@ -18,4 +18,23 @@ describe("EventCategoryRepository", () => {
 		});
 		expect(eventCategory).toBe(true);
 	});
+
+	it("Should return false on failure", async () => {
+		const sut = new EventCategoryRepository();
+		jest.spyOn(sut, "add").mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+		const eventCategory = await sut.add({
+			name: "any_name",
+			date: new Date("2021-10-10"),
+			hour: "08:10:10",
+			image: "any_image",
+			description: "any_description",
+			categories: ["any_category"],
+			street: "any_street",
+			number: 1,
+			city: "any_city",
+			state: "any_state",
+			cep: "any_cep"
+		});
+		expect(eventCategory).toBe(false);
+	});
 });
