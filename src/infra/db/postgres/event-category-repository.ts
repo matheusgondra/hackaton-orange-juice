24078b486fb401666fc78edb7c6a61656409e599
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 15:15:03 -0300

    feat: ensure EventCategoryRespository return false on failure

--- src/infra/db/postgres/event-category-repository.ts
+++ src/infra/db/postgres/event-category-repository.ts
@@ -3,41 +3,45 @@ import { db } from "./prisma";
 
 export class EventCategoryRepository implements AddEventRepository {
 	async add(newEvent: AddEventRepository.Params): Promise<AddEventRepository.Result> {
-		const eventCreated = await db.event.create({
-			data: {
-				name: newEvent.name,
-				date: newEvent.date,
-				hour: newEvent.hour,
-				image: newEvent.image,
-				description: newEvent.description,
-				address: {
-					create: {
-						street: newEvent.street,
-						number: newEvent.number,
-						city: newEvent.city,
-						state: newEvent.state,
-						zip_code: newEvent.cep
+		try {
+			const eventCreated = await db.event.create({
+				data: {
+					name: newEvent.name,
+					date: newEvent.date,
+					hour: newEvent.hour,
+					image: newEvent.image,
+					description: newEvent.description,
+					address: {
+						create: {
+							street: newEvent.street,
+							number: newEvent.number,
+							city: newEvent.city,
+							state: newEvent.state,
+							zip_code: newEvent.cep
+						}
 					}
 				}
-			}
-		});
+			});
 
-		const categoriesDb = await db.category.findMany({ where: { name: { in: newEvent.categories } } });
+			const categoriesDb = await db.category.findMany({ where: { name: { in: newEvent.categories } } });
 
-		const result = newEvent.categories.map(async (category) => {
-			const category_id = categoriesDb.find((dbCategory) => dbCategory.name === category);
-			if (category_id) {
-				return await db.eventCategory.create({
-					data: {
-						event_id: eventCreated.id,
-						category_id: category_id.id
-					}
-				});
+			const result = newEvent.categories.map(async (category) => {
+				const category_id = categoriesDb.find((dbCategory) => dbCategory.name === category);
+				if (category_id) {
+					return await db.eventCategory.create({
+						data: {
+							event_id: eventCreated.id,
+							category_id: category_id.id
+						}
+					});
+				}
+			});
+			if (result) {
+				return true;
 			}
-		});
-		if (result) {
-			return true;
+			return false;
+		} catch (error) {
+			return false;
 		}
-		return false;
 	}
 }
