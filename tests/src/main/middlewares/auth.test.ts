e5a1daafc126e3d7baa47bb028eeb7453c66b8ba
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 11:20:14 -0300

    test: ensure auth middleware return 401 if no authrization header is provided

---
+++ tests/src/main/middlewares/auth.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import app from "../../../../src/main/config/app";
+import { PrismaHelper } from "../../../helpers/prisma-helper";
+import { auth } from "../../../../src/main/middlewares";
+
+describe("Auth Middleware", () => {
+	beforeEach(async () => {
+		await PrismaHelper.connect();
+	});
+
+	afterEach(async () => {
+		await PrismaHelper.disconnect();
+	});
+
+	beforeAll(async () => {
+		app.get("/test_auth", auth, (req, res) => {
+			res.send();
+		});
+	});
+
+	it("Should return 401 if no authorization header is provided", async () => {
+		await request(app).get("/test_auth").expect(401);
+	});
+});
