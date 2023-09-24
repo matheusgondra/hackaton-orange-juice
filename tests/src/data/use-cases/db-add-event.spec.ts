e88385c3dba5dce1a603763a80f9b7772acc4148
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 13:51:36 -0300

    refactor: move sut creation to a factory method

--- tests/src/data/use-cases/db-add-event.spec.ts
+++ tests/src/data/use-cases/db-add-event.spec.ts
@@ -1,43 +1,61 @@
 import { AddEventRepository } from "../../../../src/data/protocols";
 import { DbAddEvent } from "../../../../src/data";
 
+const makeAddEventRepository = (): AddEventRepository => {
+	class AddEventRepositoryStub implements AddEventRepository {
+		async add(event: AddEventRepository.Params): Promise<AddEventRepository.Result> {
+			return {
+				id: 1,
+				name: "any_name",
+				date: new Date(),
+				hour: "any_hour",
+				image: "any_image",
+				description: "any_description",
+				categories: ["any_category"],
+				street: "any_street",
+				number: 1,
+				city: "any_city",
+				state: "any_state",
+				cep: "any_cep"
+			};
+		}
+	}
+	return new AddEventRepositoryStub();
+};
+
+interface SutTypes {
+	sut: DbAddEvent;
+	addEventRepositoryStub: AddEventRepository;
+}
+
+const makeSut = (): SutTypes => {
+	const addEventRepositoryStub = makeAddEventRepository();
+	const sut = new DbAddEvent({ addEventRepository: addEventRepositoryStub });
+	return {
+		sut,
+		addEventRepositoryStub
+	};
+};
+
+const makeFakeData = () => ({
+	name: "any_name",
+	date: new Date("2021-01-01"),
+	hour: "any_hour",
+	image: "any_image",
+	description: "any_description",
+	categories: ["any_category"],
+	street: "any_street",
+	number: 1,
+	city: "any_city",
+	state: "any_state",
+	cep: "any_cep"
+});
+
 describe("DbAddEvent", () => {
 	it("Should call addEventRepository with correct values", async () => {
-		class AddEventRepositoryStub implements AddEventRepository {
-			async add(event: AddEventRepository.Params): Promise<AddEventRepository.Result> {
-				return {
-					id: 1,
-					name: "any_name",
-					date: new Date(),
-					hour: "any_hour",
-					image: "any_image",
-					description: "any_description",
-					categories: ["any_category"],
-					street: "any_street",
-					number: 1,
-					city: "any_city",
-					state: "any_state",
-					cep: "any_cep"
-				};
-			}
-		}
-		const addEventRepositoryStub = new AddEventRepositoryStub();
-		const sut = new DbAddEvent({ addEventRepository: addEventRepositoryStub });
+		const { sut, addEventRepositoryStub } = makeSut();
 		const addSpy = jest.spyOn(addEventRepositoryStub, "add");
-		const data = {
-			name: "any_name",
-			date: new Date(),
-			hour: "any_hour",
-			image: "any_image",
-			description: "any_description",
-			categories: ["any_category"],
-			street: "any_street",
-			number: 1,
-			city: "any_city",
-			state: "any_state",
-			cep: "any_cep"
-		};
-		await sut.add(data);
-		expect(addSpy).toHaveBeenCalledWith(data);
+		await sut.add(makeFakeData());
+		expect(addSpy).toHaveBeenCalledWith(makeFakeData());
 	});
 });
