475f576fee7b0dd37a18b069dcefbc2496170588
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 10:55:49 -0300

    refactor: move sut creation to a factory method

--- tests/src/data/use-cases/db-load-administrator-by-id.spec.ts
+++ tests/src/data/use-cases/db-load-administrator-by-id.spec.ts
@@ -1,18 +1,35 @@
 import { DbLoadAdministratorById, LoadAdministratorByIdRepository } from "../../../../src/data";
 
+const makeLoadAdministratorByIdRepository = (): LoadAdministratorByIdRepository => {
+	class LoadAdministratorByIdRepositoryStub implements LoadAdministratorByIdRepository {
+		async loadById(id: number): Promise<LoadAdministratorByIdRepository.Result> {
+			return {
+				id: 1,
+				name: "Administrador",
+				password: "hashed_password"
+			};
+		}
+	}
+	return new LoadAdministratorByIdRepositoryStub();
+};
+
+interface SutTypes {
+	sut: DbLoadAdministratorById;
+	loadAdministratorByIdRepositoryStub: LoadAdministratorByIdRepository;
+}
+
+const makeSut = (): SutTypes => {
+	const loadAdministratorByIdRepositoryStub = makeLoadAdministratorByIdRepository();
+	const sut = new DbLoadAdministratorById({ loadAdministratorByIdRepository: loadAdministratorByIdRepositoryStub });
+	return {
+		sut,
+		loadAdministratorByIdRepositoryStub
+	};
+};
+
 describe("DbLoadAdministratorById", () => {
 	it("Should call loadAdministratorByIdRepository with correct value", async () => {
-		class LoadAdministratorByIdRepositoryStub implements LoadAdministratorByIdRepository {
-			async loadById(id: number): Promise<LoadAdministratorByIdRepository.Result> {
-				return {
-					id: 1,
-					name: "Administrador",
-					password: "hashed_password"
-				};
-			}
-		}
-		const loadAdministratorByIdRepositoryStub = new LoadAdministratorByIdRepositoryStub();
-		const sut = new DbLoadAdministratorById({ loadAdministratorByIdRepository: loadAdministratorByIdRepositoryStub });
+		const { sut, loadAdministratorByIdRepositoryStub } = makeSut();
 		const loadByIdSpy = jest.spyOn(loadAdministratorByIdRepositoryStub, "loadById");
 		await sut.loadById(1);
 		expect(loadByIdSpy).toBeCalledWith(1);
