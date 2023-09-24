9988f3b0ff01b0152365c9878b6d152a86114e84
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 11:05:59 -0300

    feat: ensure AdministratorRepository.loadById return an administrator on success

--- src/infra/db/postgres/administrator-repository.ts
+++ src/infra/db/postgres/administrator-repository.ts
@@ -1,9 +1,14 @@
-import { LoadAdministratorByNameRepository } from "../../../data";
+import { LoadAdministratorByIdRepository, LoadAdministratorByNameRepository } from "../../../data";
 import { db } from "./prisma";
 
-export class AdministratorRepository implements LoadAdministratorByNameRepository {
+export class AdministratorRepository implements LoadAdministratorByNameRepository, LoadAdministratorByIdRepository {
 	async loadByName(name: string): Promise<LoadAdministratorByNameRepository.Result> {
 		const administrator = await db.administrator.findUnique({ where: { name } });
+		return administrator;
+	}
+
+	async loadById(id: number): Promise<LoadAdministratorByIdRepository.Result> {
+		const administrator = await db.administrator.findUnique({ where: { id } });
 		return administrator;
 	}
 }
