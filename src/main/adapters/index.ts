5dcf7efeb19a049ee9ab5cb7f5c6123138e958d5
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 11:20:02 -0300

    feat: ensure auth middleware return 401 if no authrization header is provided

--- src/main/adapters/index.ts
+++ src/main/adapters/index.ts
@@ -1,1 +1,2 @@
 export * from "./expess-router-adapter";
+export * from "./express-middleware-adapter";
---
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -0,0 +1,13 @@
+import { AuthMiddleware } from "../../../application/middlewares";
+import { DbLoadAdministratorById } from "../../../data";
+import { JwtAdapter } from "../../../infra/cryptography";
+import { AdministratorRepository } from "../../../infra/db";
+import env from "../../config/env";
+
+export const makeAuthMiddleware = (): AuthMiddleware => {
+	const secret = env.secret;
+	const decrypter = new JwtAdapter(secret);
+	const loadAdministratorByIdRepository = new AdministratorRepository();
+	const loadAdministratorById = new DbLoadAdministratorById({ loadAdministratorByIdRepository });
+	return new AuthMiddleware({ decrypter, loadAdministratorById });
+};
