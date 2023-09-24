3196c9adef106b647ff367350c4d585cae21d550
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 15:12:28 -0300

    test: ensure EventCategoryRespository return true on success

---
+++ tests/src/infra/db/postgres/event-category-repository.spec.ts
@@ -0,0 +1,21 @@
+import { EventCategoryRepository } from "../../../../../src/infra/db";
+
+describe("EventCategoryRepository", () => {
+	it("Should return true on success", async () => {
+		const sut = new EventCategoryRepository();
+		const eventCategory = await sut.add({
+			name: "any_name",
+			date: new Date("2021-10-10"),
+			hour: "08:10:10",
+			image: "any_image",
+			description: "any_description",
+			categories: ["any_category"],
+			street: "any_street",
+			number: 1,
+			city: "any_city",
+			state: "any_state",
+			cep: "any_cep"
+		});
+		expect(eventCategory).toBe(true);
+	});
+});
