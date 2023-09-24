94395d6af812a77adf7972c10606ec7d7fc34f0c
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 10:40:28 -0300

    test: ensure JwtAdapter.decrypt call verify with correct values

--- tests/src/infra/cryptography/jwt-adapter.spec.ts
+++ tests/src/infra/cryptography/jwt-adapter.spec.ts
@@ -4,6 +4,9 @@ import jwt from "jsonwebtoken";
 jest.mock("jsonwebtoken", () => ({
 	async sign(): Promise<string> {
 		return "any_token";
+	},
+	async verify(): Promise<string> {
+		return "any_value";
 	}
 }));
 
@@ -12,25 +15,36 @@ const makeSut = (): JwtAdapter => {
 };
 
 describe("Jwt Adapter", () => {
-	it("Should call sign with correct values", () => {
-		const sut = makeSut();
-		const signSpy = jest.spyOn(jwt, "sign");
-		sut.encrypt("any_id");
-		expect(signSpy).toBeCalledWith({ id: "any_id" }, "secret");
-	});
-
-	it("Should return a token on sign success", async () => {
-		const sut = makeSut();
-		const accessToken = await sut.encrypt("any_id");
-		expect(accessToken).toBe("any_token");
-	});
-
-	it("Should throw if sign throws", async () => {
-		const sut = makeSut();
-		jest.spyOn(jwt, "sign").mockImplementationOnce(() => {
-			throw new Error();
+	describe("encrypt()", () => {
+		it("Should call sign with correct values", () => {
+			const sut = makeSut();
+			const signSpy = jest.spyOn(jwt, "sign");
+			sut.encrypt("any_id");
+			expect(signSpy).toBeCalledWith({ id: "any_id" }, "secret");
 		});
-		const promise = sut.encrypt("any_id");
-		await expect(promise).rejects.toThrow();
+
+		it("Should return a token on sign success", async () => {
+			const sut = makeSut();
+			const accessToken = await sut.encrypt("any_id");
+			expect(accessToken).toBe("any_token");
+		});
+
+		it("Should throw if sign throws", async () => {
+			const sut = makeSut();
+			jest.spyOn(jwt, "sign").mockImplementationOnce(() => {
+				throw new Error();
+			});
+			const promise = sut.encrypt("any_id");
+			await expect(promise).rejects.toThrow();
+		});
+	});
+
+	describe("decrypt()", () => {
+		it("Should call verify with correct values", async () => {
+			const sut = makeSut();
+			const verifySpy = jest.spyOn(jwt, "verify");
+			await sut.decrypt("any_token");
+			expect(verifySpy).toBeCalledWith("any_token", "secret");
+		});
 	});
 });
