37d0022b290e436a106e03b1882aa7c65bd76065
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-24 11:54:32 -0300

    refactor: move sut creation to a factory method

--- tests/src/application/controllers/event-register.spec.ts
+++ tests/src/application/controllers/event-register.spec.ts
@@ -1,30 +1,48 @@
 import { Validation } from "../../../../src/application/helpers";
 import { EventRegisterController } from "../../../../src/application/controllers";
 
+const makeValitionStub = (): Validation => {
+	class ValidationStub implements Validation {
+		validate(input: any): Validation.Result {
+			return null;
+		}
+	}
+	return new ValidationStub();
+};
+
+interface SutTypes {
+	sut: EventRegisterController;
+	validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+	const validationStub = makeValitionStub();
+	const sut = new EventRegisterController({ validation: validationStub });
+	return {
+		sut,
+		validationStub
+	};
+};
+
+const makeFakeRequest = () => ({
+	name: "any_name",
+	date: "any_date",
+	hour: "any_hour",
+	image: "any_image",
+	description: "any_description",
+	categories: ["any_category"],
+	street: "any_street",
+	number: "any_number",
+	city: "any_city",
+	state: "any_state",
+	cep: "any_cep"
+});
+
 describe("EventRegisterController", () => {
 	it("Should call validation with correct values", async () => {
-		class ValidationStub implements Validation {
-			validate(input: any): Validation.Result {
-				return null;
-			}
-		}
-		const validationStub = new ValidationStub();
-		const sut = new EventRegisterController({ validation: validationStub });
+		const { sut, validationStub } = makeSut();
 		const validateSpy = jest.spyOn(validationStub, "validate");
-		const request = {
-			name: "any_name",
-			date: "any_date",
-			hour: "any_hour",
-			image: "any_image",
-			description: "any_description",
-			categories: ["any_category"],
-			street: "any_street",
-			number: "any_number",
-			city: "any_city",
-			state: "any_state",
-			cep: "any_cep"
-		};
-		await sut.handle(request);
-		expect(validateSpy).toHaveBeenCalledWith(request);
+		await sut.handle(makeFakeRequest());
+		expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest());
 	});
 });
