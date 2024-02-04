8d4c155683a33dc43820b0cf9d5d99648d0466da
matheusgondra <matheusgondra951@gmail.com>
2024-02-04 08:43:31 -0300

    chore: update .gitignore

--- dist/main/config/middlewares.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const middlewares_1 = require("../middlewares");
-exports.default = (app) => {
-    app.use(middlewares_1.bodyParser);
-    app.use(middlewares_1.contentType);
-    app.use(middlewares_1.cors);
-};
-//# sourceMappingURL=middlewares.js.map
--- dist/main/routes/administrator-routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const factories_1 = require("../factories");
-const adapters_1 = require("../adapters");
-exports.default = (router) => {
-    router.post("/adm-login", (0, adapters_1.adaptRoute)((0, factories_1.makeLoginController)()));
-};
-//# sourceMappingURL=administrator-routes.js.map
