dca8721b5489001315a6854855c9935bda99c107
matheusgondra <matheusgondra951@gmail.com>
2024-02-04 09:18:40 -0300

    refactor: dependency injection refactoring in validators

--- src/validation/validators/required-field-validation.ts
+++ src/validation/validators/required-field-validation.ts
@@ -2,11 +2,7 @@ import { MissignParamError } from "../../application/errors";
 import { Validation } from "../../application/helpers";
 
 export class RequiredFieldValidation implements Validation {
-	private readonly fieldName: string;
-
-	constructor(fieldName: string) {
-		this.fieldName = fieldName;
-	}
+	constructor(private readonly fieldName: string) {}
 
 	validate(input: any): Validation.Result {
 		if (!input[this.fieldName]) {
